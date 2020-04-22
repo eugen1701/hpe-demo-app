f5e67bf5c723b378128a383cfec887af134305df
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-04-22 17:11:30 +0300

    user story #7474

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -5,21 +5,23 @@ public class Musical {
     public String name = "tom exception";
     public String PASSWORD = "password";
     public String PASSWORD2 = "password2";
+    public String PASSWORD3 = "password3";
     public int age;
     public int type1;
     public int type3;
     public int type2;
     public int type4;
     public int type5;
+    public int type6;
     public Musical() {
         System.out.println("not   important      constructor");
         System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaa");
-        System.out.println(PASSWORD2);
+        System.out.println(PASSWORD3);
     }
 
 
     public void setMusical() throws ExceptionInInitializerError {
-        throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type2 + type3 + type4 + type5 + PASSWORD);
+        throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type6 + type1 + type2 + type3 + type4 + type5 + PASSWORD);
     }
 
 }
