2c86e06667244550e529ced6dfc8f0211b7c754f
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-04-28 01:08:52 +0300

    defect #1002

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -6,6 +6,7 @@ public class Musical {
     public String PASSWORD = "password";
     public String PASSWORD6 = "password6";
     public String PASSWORD8 = "password8";
+    public String PASSWORD10 = "password10";
     public int age;
     public int type1;
     public int type3;
@@ -19,7 +20,7 @@ public class Musical {
 
 
     public void setMusical() throws ExceptionInInitializerError {
-        throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type2 + type11);
+        throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type2 + type11 + PASSWORD10);
     }
 
 }
