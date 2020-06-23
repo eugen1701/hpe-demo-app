202480f18cf3e4ad98dba762aa6340fcac5c40cd
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-06-23 09:46:46 +0300

    user story #2001

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -16,7 +16,7 @@ public class Band {
 	public String name = "";
 	public String logo = "1";
 	public String song = "1";
-	public int votes = 2;
+	public int votes = 3;
 
 	public Band(JSONObject json) {
 		if (json.has("id1")) {
