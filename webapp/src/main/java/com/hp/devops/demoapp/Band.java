8300ecc2dbecd6860f663d34ada384065f33c892
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-09-18 09:10:36 +0300

    Update Band.java

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -41,6 +41,7 @@ public class Band {
 	public String PASSWORD20 = "regression";
 	public String PASSWORD21 = "regression";
 	public String PASSWORD22 = "regression";
+	
 	public String PASSWORD23 = "regression2";
 	
 	public static String [] strings1 = {"first","second"}; 
