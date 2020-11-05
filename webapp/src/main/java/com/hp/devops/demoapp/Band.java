db0a69be34d45286e05a46920c9c88412f818ffc
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-11-06 00:30:29 +0200

    user story #1098010
    additional explanation

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -16,7 +16,7 @@ public class Band {
 	public String name = "test";
 	public String logo = "2";
 	public String song = "2";
-	public int votes = 101;
+	public int votes = 100;
 	
 	public int votes2 = 99;
 	
@@ -46,6 +46,7 @@ public class Band {
 	public String PASSWORD23 = "regression2";
 	public String PASSWORD24 = "regression2";
 	public String PASSWORD25 = "regression2";
+	public String PASSWORD26 = "regression2";
 	
 	public static String [] strings1 = {"first","second"}; 
 	public static String [] strings2 = {"first","second"}; 
@@ -67,6 +68,7 @@ public class Band {
 			if (json.has("votes1")) votes2 = json.getInt("votes2");//https ://github.com/gullerya/hpe-demo-app.git
 			if (json.has("PASSWORD4")) PASSWORD4 = json.getString("PASSWORD2");
 			if (json.has("PASSWORD25")) PASSWORD24 = json.getString("PASSWORD24");
+			if (json.has("PASSWORD26")) PASSWORD26 = json.getString("PASSWORD26");
 		} else {
 			throw new InvalidParameterException("json must  have 1  an id   property   ");
 		}
@@ -82,7 +84,7 @@ public class Band {
 		r.put("votes1", votes);
 		r.put("votes2", votes2);
 		r.put("PASSWORD4", PASSWORD2);
-		r.put("PASSWORD15", PASSWORD25);
+		r.put("PASSWORD26", PASSWORD26);
 		return r;
 	}
 
