7040524a19dcd7cd956b12acf73f0c4c96c367cc
lokshin <evgene.lokshin@microfocus.com>
2020-11-18 10:03:36 +0200

    my change

--- ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestD.java
+++ ui-tests/src/test/java/com/hp/devops/demoapp/tests/ui/TestD.java
@@ -8,7 +8,6 @@ import org.junit.Test;
  *
  */
 public class TestD {
-	//comment
 	@Test
 	public void testUIcaseAlwaysPass() {
 		System.out.println("Proudly running test " + Thread.currentThread().getStackTrace()[1]);
