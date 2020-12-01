445ba200044132271ef9d6d1e992d49239383826
iulia-andreicut <iulia.andreicut@microfocus.com>
2020-12-01 14:30:08 +0200

    user story #1003

--- webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Calcs.java
@@ -9,6 +9,7 @@ public class Calcs {
 
 	public static int sum(int a, int b) {     //  1 test covers this
 		return a + b;
+		System.out.println(a.toString());
 	}
 
 	public static int sub(int a, int b) {     //  3 tests cover this
