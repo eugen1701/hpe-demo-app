7919549bfe5f65bb8f882b19197f923b3d5d91f0
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-02-10 13:53:19 +0200

    Update Band.java

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -57,6 +57,7 @@ public class Band {
 	public String PASSWORD33 = "regression2";
 	public String PASSWORD34 = "regression2";
 	public String PASSWORD35 = "regression35";
+	public String PASSWORD36 = "regression36";
 	
 	public static String [] strings1 = {"first","second"}; 
 	public static String [] strings2 = {"first","second"}; 
@@ -79,7 +80,7 @@ public class Band {
 			if (json.has("votes1")) votes2 = json.getInt("votes2");//https ://github.com/gullerya/hpe-demo-app.git
 			if (json.has("PASSWORD4")) PASSWORD4 = json.getString("PASSWORD2");
 			if (json.has("PASSWORD4")) PASSWORD24 = json.getString("PASSWORD24");
-			if (json.has("regression2")) PASSWORD35 = json.getString("PASSWORD35");
+			if (json.has("regression2")) PASSWORD36 = json.getString("PASSWORD36");
 		} else {
 			throw new InvalidParameterException("json must  have 1  an id   property   ");
 		}
@@ -95,7 +96,7 @@ public class Band {
 		r.put("votes1", votes);
 		r.put("votes2", votes2);
 		r.put("PASSWORD4", PASSWORD2);
-		r.put("PASSWORD35", PASSWORD35);
+		r.put("PASSWORD36", PASSWORD36);
 		return r;
 	}
 
