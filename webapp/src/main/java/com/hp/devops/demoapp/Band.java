318a9bb59b3f675756722de04cf28253c8794cc3
iulia-andreicut <iulia.andreicut@microfocus.com>
2021-07-07 15:00:02 +0300

    user story #2022

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -64,6 +64,7 @@ public class Band {
 	public String PASSWORD58 = "regression45";
 	public String PASSWORD59 = "regression37";
 	public String PASSWORD60 = "regression360";
+	public String PASSWORD61 = "regression360";
 
 
 	public Band(JSONObject json) {
@@ -76,7 +77,7 @@ public class Band {
 			if (json.has("votes1")) votes2 = json.getInt("votes2");//https ://github.com/gullerya/hpe-demo-app.git
 			if (json.has("PASSWORD4")) PASSWORD4 = json.getString("PASSWORD2");
 			if (json.has("PASSWORD4")) PASSWORD24 = json.getString("PASSWORD24");
-			if (json.has("PASSWORD360")) PASSWORD60 = json.getString("PASSWORD58");
+			if (json.has("PASSWORD360")) PASSWORD61 = json.getString("PASSWORD58");
 		} else {
 			throw new InvalidParameterException("json must  have 1  an id   property   ");
 		}
@@ -93,7 +94,7 @@ public class Band {
 		r.put("votes2", votes2);
 		r.put("PASSWORD4", PASSWORD2);
 		r.put("PASSWORD58", PASSWORD58);
-		r.put("PASSWORD60", PASSWORD59);
+		r.put("PASSWORD60", PASSWORD61);
 		return r;
 	}
 
