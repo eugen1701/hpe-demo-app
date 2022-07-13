939292cddeff73d577564f693b052908ebb1e22a
eugen1701 <56292405+eugen1701@users.noreply.github.com>
2022-07-13 14:37:13 +0300

    oneCommitFromTestBranch

--- webapp/src/main/java/com/hp/devops/demoapp/Band.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Band.java
@@ -11,7 +11,7 @@ import java.security.InvalidParameterException;
  * Time: 10:03 Zeugnis testing again
  * To change this template use File  | Settings | File Templates.
  * test67
- * One Comment
+ * One Comment edited
  */
 public class Band {
 	public int id;
